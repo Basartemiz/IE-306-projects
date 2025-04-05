6bb8a3f68f619044629c415c1e6787628c7182f3
AHMET-CAGDAS-GIRIT <141752766+AHMET-CAGDAS-GIRIT@users.noreply.github.com>
2025-04-05 19:50:41 +0300

    Update IE306HW1.py

--- project1/IE306HW1.py
+++ project1/IE306HW1.py
@@ -1,11 +1,17 @@
+import numpy # random didn't have normal distibution in it so we had to use numpy
 import random
 import math
 
 # We will create all the parameters we need.
 
-seed = 1232 # It will be use to generate the same pseudo number over and over again.
+run_multiple_times = True # To detertmine whether we run the simulation only once or not.
+number_of_runs = 20 # only relevant if we are running it multiple times.
+confidence_level = 0.95 # The confidence interval of our results if we run multiple times.
+
+# the seed : 4044800276
+seed = 4044800276 # It will be use to generate the same pseudo number over and over again.
 random.seed(seed)
-arrival_amount = 1000 # The number of clients that arrive to the hospital.
+arrival_amount = 200 # The number of clients that arrive to the hospital.
                       # This is used as our simulation is a discrete event simulation.
 S = 3 # Number of nurses.
 mu_a = 1.0 # Average patient arrival rate.
@@ -16,6 +22,9 @@ alpha = 1.0 # Alpha belong to an uniform distribution
 mu_cb = 0.15625 # Average healing rate at hospital.
 p1 = 0.25 # The chance of patient going home instead of hospital.
 hospital_capaticy = 6 # Number of beds in hospital
+
+
+
 
 # Now we will define variables that will be used to run the simulation.
 
@@ -35,6 +44,15 @@ average_nurse_utilization = 0.0
 average_occupied_bed = 0.0
 patient_going_home_ratio = 0.0
 average_healing_time = 0.0
+
+# We also need the mean and S values of these output variables. To do that we will put them in lists.
+triage_or_hospital_empty_ratio_list = []
+triage_and_hospital_empty_ratio_list = []
+critical_going_home_ratio_list = []
+average_nurse_utilization_list = []
+average_occupied_bed_list = []
+patient_going_home_ratio_list = []
+average_healing_time_list= []
 
 # Now we will define variables that will be used to calculate outputs.
 current_time = 0.0
@@ -88,6 +106,22 @@ def Display_Future_Event_List(): # This function is added for the sule purpose o
         time,code = element
         print("time:",time,"\tevent:",event_names[code])
     print("---")
+    
+def Display_Current_State(): # This function exists to make displaying output easier
+    global current_time
+    global number_of_patients_to_be_evaluated
+    global number_of_patients_in_nurse_queue
+    global number_of_patients_at_home
+    global number_of_patients_at_hospital
+    print("------------------")
+    Display_Future_Event_List()
+    print("current time:",current_time)
+    print("number of patients to be evaluated:",number_of_patients_to_be_evaluated)
+    print("number of patients in nurse queue:",number_of_patients_in_nurse_queue)
+    print("number of patients at home:",number_of_patients_at_home)
+    print("number of patients at hospital:",number_of_patients_at_hospital)
+    print("------------------")
+    return
 
 def Add_Event(time,code): # time is the time of the event , code is the type of the event.
     future_event_list.append((time,code))
@@ -210,45 +244,181 @@ def Execute_Event(): # Iterates through the future event list and executes the n
     pass
 
 
+    
 
- 
-Add_Event(Generate_Interarrival(),1)
-print("------------------")
-Display_Future_Event_List()
-print("current time:",current_time)
-print("number of patients to be evaluated:",number_of_patients_to_be_evaluated)
-print("number of patients in nurse queue:",number_of_patients_in_nurse_queue)
-print("number of patients at home:",number_of_patients_at_home)
-print("number of patients at hospital:",number_of_patients_at_hospital)
-print("------------------")
+# Now we will determine whether we will run the simulation once or multiple time.
 
-while(Advance_Time()):  
+if (run_multiple_times == False):
+    current_time = 0.0 # To reset the simulation.
+    Add_Event(Generate_Interarrival(),1)
+    Display_Current_State()
+
+    while(Advance_Time()):  
+        Display_Current_State()
+    
+    triage_or_hospital_empty_ratio = amount_of_time_triage_or_hospital_is_empty/current_time
+    triage_and_hospital_empty_ratio = amount_of_time_triage_and_hospital_is_empty/current_time
+    critical_going_home_ratio = total_number_of_critical_patinets_going_home/total_number_of_critical_patinets
+    patient_going_home_ratio = total_number_of_patinets_treated_at_home/total_number_of_patinets_treated
+    average_occupied_bed = total_occupied_bed_times_time/current_time
+    
+    print("Results:")
+    print("triage or hospital empty ratio:",triage_or_hospital_empty_ratio)
+    print("triage and hospital empty ratio:",triage_and_hospital_empty_ratio)
+    print("critical going home ratio:",critical_going_home_ratio)
+    print("average nurse utilization:",average_nurse_utilization)
+    print("average occupied bed:",average_occupied_bed)
+    print("patient going home ratio:",patient_going_home_ratio)
+    print("average healing time:",average_healing_time)
+else: # If there are multiple runs, we will calculate the mean and S value of outputs to create a confidence interval
+    number_of_runs_executed = 0 # it determines how many runs have been executed so far. also used for seed generation.
+    while(number_of_runs_executed < number_of_runs):
+        random.seed(seed+number_of_runs_executed) # To reset the seed.
+        number_of_patients_to_arrive = arrival_amount # To reset the patients.
+        current_time = 0.0 # To reset the simulation.
+        # Now we will reset all the variables that are used to calculate the outputs and the outputs themselves
+        triage_or_hospital_empty_ratio = 0.0
+        triage_and_hospital_empty_ratio = 0.0
+        critical_going_home_ratio = 0.0
+        average_nurse_utilization = 0.0
+        average_occupied_bed = 0.0
+        patient_going_home_ratio = 0.0
+        average_healing_time = 0.0
+        
+        amount_of_time_triage_or_hospital_is_empty = 0.0
+        amount_of_time_triage_and_hospital_is_empty = 0.0
+        total_occupied_bed_times_time = 0.0
+        total_number_of_patinets_treated = 0
+        total_number_of_patinets_treated_at_home = 0
+        total_number_of_critical_patinets = 0
+        total_number_of_critical_patinets_going_home = 0
+        Add_Event(Generate_Interarrival(),1)
+        while(Advance_Time()): # We are advancing through events  
+            pass
+        # Now we will calculate the output of each run.
+        triage_or_hospital_empty_ratio = amount_of_time_triage_or_hospital_is_empty/current_time
+        triage_and_hospital_empty_ratio = amount_of_time_triage_and_hospital_is_empty/current_time
+        critical_going_home_ratio = total_number_of_critical_patinets_going_home/total_number_of_critical_patinets
+        patient_going_home_ratio = total_number_of_patinets_treated_at_home/total_number_of_patinets_treated
+        average_occupied_bed = total_occupied_bed_times_time/current_time
+        
+        # Then we will fill our output lists.
+        triage_or_hospital_empty_ratio_list.append(triage_or_hospital_empty_ratio)
+        triage_and_hospital_empty_ratio_list.append(triage_and_hospital_empty_ratio)
+        critical_going_home_ratio_list.append(critical_going_home_ratio)
+        average_nurse_utilization_list.append(average_nurse_utilization)
+        average_occupied_bed_list.append(average_occupied_bed)
+        patient_going_home_ratio_list.append(patient_going_home_ratio)
+        average_healing_time_list.append(average_healing_time)
+        
+        number_of_runs_executed += 1
+        
+    # Now it's time to calculate the means , S values and confidence intervals.
+    triage_or_hospital_empty_ratio_mean = 0.0
+    triage_and_hospital_empty_ratio_mean = 0.0
+    critical_going_home_ratio_mean = 0.0
+    average_nurse_utilization_mean = 0.0
+    average_occupied_bed_mean = 0.0
+    patient_going_home_ratio_mean = 0.0
+    average_healing_time_mean = 0.0
+    
+    triage_or_hospital_empty_ratio_variance = 0.0
+    triage_and_hospital_empty_ratio_variance = 0.0
+    critical_going_home_ratio_variance = 0.0
+    average_nurse_utilization_variance = 0.0
+    average_occupied_bed_variance = 0.0
+    patient_going_home_ratio_variance = 0.0
+    average_healing_time_variance = 0.0
+    
+    triage_or_hospital_empty_ratio_interval = [0.0,0.0]
+    triage_and_hospital_empty_ratio_interval = [0.0,0.0]
+    critical_going_home_ratio_interval = [0.0,0.0]
+    average_nurse_utilization_interval = [0.0,0.0]
+    average_occupied_bed_interval = [0.0,0.0]
+    patient_going_home_ratio_interval = [0.0,0.0]
+    average_healing_time_interval = [0.0,0.0]
+    
+    # Now we will calculate means.
+    
+    for i in range(number_of_runs):
+        triage_or_hospital_empty_ratio_mean += triage_or_hospital_empty_ratio_list[i]/number_of_runs
+        triage_and_hospital_empty_ratio_mean += triage_and_hospital_empty_ratio_list[i]/number_of_runs
+        critical_going_home_ratio_mean += critical_going_home_ratio_list[i]/number_of_runs
+        average_nurse_utilization_mean += average_nurse_utilization_list[i]/number_of_runs
+        average_occupied_bed_mean += average_occupied_bed_list[i]/number_of_runs
+        patient_going_home_ratio_mean += patient_going_home_ratio_list[i]/number_of_runs
+        average_healing_time_mean += average_healing_time_list[i]/number_of_runs
+        
+    # Now we will calculate S values.
+        
+    for i in range(number_of_runs):
+        triage_or_hospital_empty_ratio_variance += (triage_or_hospital_empty_ratio_mean - triage_or_hospital_empty_ratio_list[i])**2 / (number_of_runs-1)
+        triage_and_hospital_empty_ratio_variance += (triage_and_hospital_empty_ratio_mean - triage_and_hospital_empty_ratio_list[i])**2 / (number_of_runs-1)
+        critical_going_home_ratio_variance += (critical_going_home_ratio_mean - critical_going_home_ratio_list[i])**2 / (number_of_runs-1)
+        average_nurse_utilization_variance += (average_nurse_utilization_mean - average_nurse_utilization_list[i])**2 / (number_of_runs-1)
+        average_occupied_bed_variance += (average_occupied_bed_mean - average_occupied_bed_list[i])**2 / (number_of_runs-1)
+        patient_going_home_ratio_variance += (patient_going_home_ratio_mean -  patient_going_home_ratio_list[i])**2 / (number_of_runs-1)
+        average_healing_time_variance += (average_healing_time_mean - average_healing_time_list[i])**2 / (number_of_runs-1)
+    
+    triage_or_hospital_empty_ratio_variance = math.sqrt(triage_or_hospital_empty_ratio_variance)
+    triage_and_hospital_empty_ratio_variance = math.sqrt(triage_and_hospital_empty_ratio_variance)
+    critical_going_home_ratio_variance = math.sqrt(critical_going_home_ratio_variance)
+    average_nurse_utilization_variance = math.sqrt(average_nurse_utilization_variance)
+    average_occupied_bed_variance = math.sqrt(average_occupied_bed_variance)
+    patient_going_home_ratio_variance = math.sqrt(patient_going_home_ratio_variance)
+    average_healing_time_variance = math.sqrt(average_healing_time_variance)
+    
+    # Now we will calculate the confidence intervals. We will approximate T distibutions with normal distribution.
+    z_value = numpy.random.normal((1-confidence_level)/2) # To not calculate to same value over and over again
+    
+    triage_or_hospital_empty_ratio_interval[0] = triage_or_hospital_empty_ratio_mean - z_value*(triage_or_hospital_empty_ratio_variance/math.sqrt(number_of_runs))
+    triage_or_hospital_empty_ratio_interval[1] = triage_or_hospital_empty_ratio_mean + z_value*(triage_or_hospital_empty_ratio_variance/math.sqrt(number_of_runs))
+    
+    triage_and_hospital_empty_ratio_interval[0] =  triage_and_hospital_empty_ratio_mean - z_value*(triage_and_hospital_empty_ratio_variance/math.sqrt(number_of_runs))
+    triage_and_hospital_empty_ratio_interval[1] = triage_and_hospital_empty_ratio_mean + z_value*(triage_and_hospital_empty_ratio_variance/math.sqrt(number_of_runs))
+    
+    critical_going_home_ratio_interval[0] = critical_going_home_ratio_mean - z_value*(critical_going_home_ratio_variance/math.sqrt(number_of_runs))
+    critical_going_home_ratio_interval[1] = critical_going_home_ratio_mean + z_value*(critical_going_home_ratio_variance/math.sqrt(number_of_runs))
+    
+    average_nurse_utilization_interval[0] = average_nurse_utilization_mean - z_value*(average_nurse_utilization_variance/math.sqrt(number_of_runs))
+    average_nurse_utilization_interval[1] = average_nurse_utilization_mean + z_value*(average_nurse_utilization_variance/math.sqrt(number_of_runs))
+    
+    average_occupied_bed_interval[0] = average_occupied_bed_mean - z_value*(average_occupied_bed_variance/math.sqrt(number_of_runs))
+    average_occupied_bed_interval[1] = average_occupied_bed_mean + z_value*(average_occupied_bed_variance/math.sqrt(number_of_runs))
+    
+    patient_going_home_ratio_interval[0] = patient_going_home_ratio_mean - z_value*(patient_going_home_ratio_variance/math.sqrt(number_of_runs))
+    patient_going_home_ratio_interval[1] = patient_going_home_ratio_mean + z_value*(patient_going_home_ratio_variance/math.sqrt(number_of_runs))
+    
+    average_healing_time_interval[0] = average_healing_time_mean - z_value*(average_healing_time_variance/math.sqrt(number_of_runs))
+    average_healing_time_interval[1] = average_healing_time_mean + z_value*(average_healing_time_variance/math.sqrt(number_of_runs))
+    
+    print("Results after",number_of_runs,"runs:")
+    print("mean triage or hospital empty ratio:",triage_or_hospital_empty_ratio_mean)
+    print("mean triage and hospital empty ratio:",triage_and_hospital_empty_ratio_mean)
+    print("mean critical going home ratio:",critical_going_home_ratio_mean)
+    print("mean average nurse utilization:",average_nurse_utilization_mean)
+    print("mean average occupied bed:",average_occupied_bed_mean)
+    print("mean patient going home ratio:",patient_going_home_ratio_mean)
+    print("mean average healing time:",average_healing_time_mean)
+    print("---")
+    print("triage or hospital empty ratio variance:",triage_or_hospital_empty_ratio_variance)
+    print("triage and hospital empty ratio variance:",triage_and_hospital_empty_ratio_variance)
+    print("critical going home ratio variance:",critical_going_home_ratio_variance)
+    print("average nurse utilization variance:",average_nurse_utilization_variance)
+    print("average occupied bed variance:",average_occupied_bed_variance)
+    print("patient going home ratio variance:",patient_going_home_ratio_variance)
+    print("average healing time variance:",average_healing_time_variance)
     print("------------------")
-    Display_Future_Event_List()
-    print("current time:",current_time)
-    print("number of patients to be evaluated:",number_of_patients_to_be_evaluated)
-    print("number of patients in nurse queue:",number_of_patients_in_nurse_queue)
-    print("number of patients at home:",number_of_patients_at_home)
-    print("number of patients at hospital:",number_of_patients_at_hospital)
-    print("------------------")
+    # To calculate the interval we should use T disribution . But we can approximate by using the normal distibution.
+    print(f"The confidence intervals with %{int(confidence_level*100)} condifence level:")
+    print(f"triage or hospital empty ratio interval: [{triage_or_hospital_empty_ratio_interval[0]},{triage_or_hospital_empty_ratio_interval[1]}]")
+    print(f"triage and hospital empty ratio interval: [{triage_and_hospital_empty_ratio_interval[0]},{triage_and_hospital_empty_ratio_interval[1]}]")
+    print(f"critical going home ratio interval: [{critical_going_home_ratio_interval[0]},{critical_going_home_ratio_interval[1]}]")
+    print(f"average nurse utilization interval: [{average_nurse_utilization_interval[0]},{average_nurse_utilization_interval[1]}]")
+    print(f"average occupied bed interval: [{average_occupied_bed_interval[0]},{average_occupied_bed_interval[1]}]")
+    print(f"patient going home ratio interval: [{patient_going_home_ratio_interval[0]},{patient_going_home_ratio_interval[1]}]")
+    print(f"average healing time interval: [{average_healing_time_interval[0]},{average_healing_time_interval[1]}]")
     
-triage_or_hospital_empty_ratio = amount_of_time_triage_or_hospital_is_empty/current_time
-
-triage_and_hospital_empty_ratio = amount_of_time_triage_and_hospital_is_empty/current_time
-    
-critical_going_home_ratio = total_number_of_critical_patinets_going_home/total_number_of_critical_patinets
-    
-patient_going_home_ratio = total_number_of_patinets_treated_at_home/total_number_of_patinets_treated
-
-average_occupied_bed = total_occupied_bed_times_time/current_time
-    
-print("Results:")
-print("triage or hospital empty ratio:",triage_or_hospital_empty_ratio)
-print("triage and hospital empty ratio:",triage_and_hospital_empty_ratio)
-print("critical going home ratio:",critical_going_home_ratio)
-print("average nurse utilization:",average_nurse_utilization)
-print("average occupied bed:",average_occupied_bed)
-print("patient going home ratio:",patient_going_home_ratio)
-print("average healing time:",average_healing_time)
+       
     
     
